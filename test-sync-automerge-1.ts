7696b9e3597a4d283ed220cb717702e1f2f722ee
Philip Mendels <p.mendels@gmail.com>
2023-01-10 00:34:22 +0100

    Add test for Automerge 1

--- test-sync-automerge-1.ts
+++ test-sync-automerge-1.ts
@@ -6,38 +6,28 @@ import {
   from,
   generateSyncMessage,
   getLastLocalChange,
-  SyncMessage,
+  BinarySyncMessage as SyncMessage,
   receiveSyncMessage,
-  decodeSyncMessage,
+  Backend,
   getAllChanges,
   decodeChange,
-  Change,
-} from "@automerge/automerge";
+  BinaryChange as Change,
+} from "automerge";
 
-// Start with two docs with the same initial change:
+const { decodeSyncMessage } = Backend;
+
 let docA = from({ counter: 0 });
 let docB = clone(docA);
 
-// Create some changes in the second doc:
 for (let i = 0; i < 5; i++) {
   docB = change(docB, (doc) => doc.counter++);
 }
 
-// Apply the last change of the second doc to the first doc,
-// so that the first doc has a 'gap' (missing in-between changes).
-// These missing changes seem to be synced one-by-one.
 [docA] = applyChanges(docA, [getLastLocalChange(docB)!]);
-
-// Optionally create more changes in the second doc. Due to the gap
-// above these changes seem to be synced one-by-one as well starting
-// from the end, therefore possibly creating a new gap.
-//
-// for (let i = 0; i < 10; i++) {
-//   docB = change(docB, (doc) => doc.counter++);
-// }
 
 const getHash = (change: Change) => decodeChange(change).hash;
 
+console.log("docA hashes", getAllChanges(docA).map(getHash));
 console.log("docB hashes", getAllChanges(docB).map(getHash));
 
 const makeClient = (initialDoc: typeof docA) => {
@@ -68,7 +58,10 @@ const logMessage = (name: string, message: SyncMessage) => {
   console.log(name, { changeHashes: changes.map(getHash), ...rest });
 };
 
-const sync = (iteration: number, messageForB: SyncMessage | null): number => {
+const sync = (
+  iteration: number,
+  messageForB: SyncMessage | null | undefined
+): number => {
   console.log("");
   console.log("iteration", iteration);
   if (messageForB) {
